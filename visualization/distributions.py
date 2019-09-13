4a88d89a506a08abf2de458ee55f2ff3932f66e2
Mantraraj <mantraraj.dash@zs.com>
2019-09-13 20:58:19 +0530

    added histogram and bar charts

--- visualization/distributions.py
+++ visualization/distributions.py
@@ -1,14 +1,29 @@
 """Perform useful descriptive visualizations of variable distributions
 
 Classes:
+-----------------
 VarDistributions -- Plot distributions of various variables given a dataframe
 """
 
 import pandas as pd
 import numpy as np
 
+import ipywidgets as widgets
+from ipywidgets import HBox, Layout, Output, VBox
+from IPython.display import display, clear_output
+import qgrid
 
-class VarDistributions():
+import plotly
+import plotly.graph_objs as go
+import plotly.offline as pyo
+import plotly.express as px
+import plotly.figure_factory as ff
+
+# Set plotly notebook mode to work offline
+pyo.init_notebook_mode()
+
+
+class VarPlots():
     """Plot distributions of various variables given a dataframe
 
     Functions:
@@ -16,32 +31,103 @@ class VarDistributions():
     plot_box -- boxplot for continuous variables
     """
 
-    def __init__(self, df, plot_vars):
+    def __init__(self, df):
         # input dataframe and variables to plot distributions of
 
+        self.widget_style = {'description_width': 'initial',
+                             'widget_width': 'initial'}
         self.data = df
-        self.var_list = plot_vars
+        self.var_list = self.data.columns.values
 
         # extract column datatypes
         self.col_types = {col: self.data[col].dtype.name
                           for col in self.data.columns.values}
 
-        # assert datatypes can be plotted
-        self.error_flag = False
-        self.obj_cols = []
-        for col in self.var_list:
-            if self.col_types[col] == 'object':
-                self.obj_cols.append(col)
-                self.error_flag = True
-        obj_cols_string = '\n'.join([x + '\n' for x in self.obj_cols])
-        assert ~self.error_flag,\
-            f"Object columns cannot be plotted:{obj_cols_string}"
+    # TODO add functions for box plot
+    def counts_by_column(self):
+        # plot histogram or barplot based on input column data type
 
-        # print info about columns not being plotted
-        cols_not_plotted = '\n'.join([x + ':' + self.col_types[col] + '\n'
-                                     for x in self.data.columns.values
-                                     if x not in self.var_list])
-        print(f'List of variables excluded:{cols_not_plotted}')
+        # TODO testing
+        button = widgets.Button(description="Submit",
+                                style=self.widget_style)
 
-        # TODO test code above
-        # TODO add functions for histogram, box plot
+        col_picker = widgets.Dropdown(
+                                        options=[(x, x)
+                                                    for x in self.var_list],
+                                        value=self.var_list[0],
+                                        description='Select Column:',
+                                        style=self.widget_style
+                                    )
+        chart_picker = widgets.Dropdown(
+                                        options=["Bar", "Histogram"],
+                                        value="Bar",
+                                        description='Select Plot Type:',
+                                        style=self.widget_style
+                                        )                            
+        container = widgets.VBox(children=[col_picker, chart_picker,
+                                            button])
+        display(container)
+        
+        def process_data(v):
+            # process data and display chart on button click
+            
+            clear_output()
+
+            select_col = col_picker.value
+            select_chart = chart_picker.value
+            
+            # plot histogram
+            fig = go.Figure()
+            figx = self.data[select_col].sort_values()
+            if len(figx) == np.sum(pd.isnull(figx)):
+                string_display = '<br><h4>Column contains only'+\
+                                    'null values</h4>'
+                text_widg = widgets.HTML(value=string_display)
+                container = widgets.VBox(children=[col_picker,
+                                                    chart_picker, button,
+                                                    text_widg])
+                return
+            
+            # TODO make modular
+            if figx.dtype != 'object':
+                figx = figx.fillna(0).values
+            else:
+                figx = figx.fillna('NA').values
+            unique, counts = np.unique(figx, return_counts=True)
+            
+            if select_chart == 'Bar':
+                # bin values and plot out charts
+                if len(unique) > 7:
+                    hist, bins = np.histogram(figx, bins=7)
+                    labels = ['{0:.0f}'.format(bins[i]) + '-'
+                                + '{0:.0f}'.format(bins[i+1])
+                                for i in range(len(bins) - 1)]
+                    fig.add_trace(go.Bar(x=labels, y=hist))
+                    fig.layout = go.Layout(title={'text': 'Count of records by '+select_col.capitalize().replace('_',' '),
+                                                'x': 0.5,
+                                                'xanchor': 'center'})
+                else: 
+                    fig.add_trace(go.Bar(x=unique, y=counts))
+                    fig.layout = go.Layout(title={'text': 'Count of records by '+select_col.capitalize().replace('_',' '),
+                                                'x': 0.5,
+                                                'xanchor': 'center'})
+            else:
+                # assert object data type can be plotted in histogram
+                assert self.col_types[select_col] != 'object',\
+                    "Object columns cannot be plotted in histogram."\
+                        + "Please reselect the column"
+                
+                # plot histogram
+                fig.add_trace(go.Histogram(x=figx, histfunc='count'))
+                fig.layout = go.Layout(title={'text': 'Count of records by '+select_col.capitalize().replace('_',' '),
+                                            'x': 0.5,
+                                            'xanchor': 'center'})
+            string_display = f'<br><h4>Number of unique values: {len(unique)}</h4>'
+            text_widg = widgets.HTML(value=string_display)
+            container = widgets.VBox(children=[col_picker,
+                                                chart_picker, button,
+                                                text_widg,
+                                                go.FigureWidget(fig)])
+            display(container)
+        
+        button.on_click(process_data)
